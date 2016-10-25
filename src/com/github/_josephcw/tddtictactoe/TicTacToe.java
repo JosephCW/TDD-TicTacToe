a600a5639842b70d98442b70aca4654f55a6e60d
Joseph Watts <josephcw@live.com>
2016-10-25 09:57:27 -0500

    Ensure piece is played on the board.

--- src/com/github/_josephcw/tddtictactoe/TicTacToe.java
+++ src/com/github/_josephcw/tddtictactoe/TicTacToe.java
@@ -1,5 +1,11 @@
 package com.github._josephcw.tddtictactoe;
 
 public class TicTacToe {
-
+	public void play(int x, int y) {
+		if(x < 1 || x > 3) {
+			throw new RuntimeException();
+		} else if(y < 1 || y > 3) {
+			throw new RuntimeException();
+		}
+	}
 }
--- test/com/github/_josephcw/tddtictactoe/TicTacToeTest.java
+++ test/com/github/_josephcw/tddtictactoe/TicTacToeTest.java
@@ -1,5 +1,30 @@
 package com.github._josephcw.tddtictactoe;
 
-public class TicTacToeTest {
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
+public class TicTacToeTest {
+	@Rule
+	public ExpectedException e = ExpectedException.none();
+	
+	private TicTacToe ticTacToe;
+	
+	@Before
+	public final void before() {
+		ticTacToe = new TicTacToe();
+	}
+	
+	@Test
+	public void whenXOutsideBoardLimitsThenRuntimeException() {
+		e.expect(RuntimeException.class);
+		ticTacToe.play(4, 1);
+	}
+	
+	@Test
+	public void whenYOutsideBoardLimitsThenRuntimeException() {
+		e.expect(RuntimeException.class);
+		ticTacToe.play(1, 4);
+	}
 }
